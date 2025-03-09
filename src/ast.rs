2194e81b30bdf01c5709e9d56728677d70ec17b9
f2koi <flarefinkoi@gmail.com>
2025-03-10 03:31:50 +0900

    Support parsing lines with implicit ports for certain schemes like HTTPS

--- src/ast.rs
+++ src/ast.rs
@@ -1,27 +1,56 @@
-use std::net::SocketAddrV4;
+use std::net::Ipv4Addr;
 
 #[derive(Debug)]
 pub struct HttpSpec {
-    pub tailscale_listen_addresses: Vec<ListenAddress>,
+    pub tailscale_listen_addresses: Vec<ListenAddressWithProtocol>,
     pub proxy_target_port: u16,
 }
 
 #[derive(Debug)]
 pub struct HttpsSpec {
     pub mode: OperationMode,
-    pub tailscale_listen_addresses: Vec<ListenAddress>,
+    pub tailscale_listen_addresses: Vec<ListenAddressWithProtocol>,
     pub proxy_target_port: u16,
 }
 
 #[derive(Debug)]
-pub enum ListenAddress {
-    Domain { domain_name: String, port: u16 },
-    Ipv4(SocketAddrV4),
-    Ipv6 { port: u16 }, // Parsing IPv6 is toooooooooooooooooooooooooooo0ooOoo00Ooo hard
+pub struct ListenAddressWithProtocol {
+    pub protocol: Protocol,
+    pub listen_address: ListenAddress,
+}
+
+#[derive(Debug)]
+pub struct ListenAddress {
+    pub domain_or_ip: DomainOrIp,
+    pub port: u16,
+}
+
+#[derive(Debug)]
+pub enum DomainOrIp {
+    Domain { domain_name: String },
+    Ipv4(Ipv4Addr),
+    Ipv6, // Parsing IPv6 is toooooooooooooooooooooooooooo0ooOoo00Ooo hard
 }
 
 #[derive(Debug)]
 pub enum OperationMode {
     Funnel,
     Serve,
+}
+
+#[derive(Debug)]
+pub enum Protocol {
+    Http,
+    Https,
+    Tcp,
+}
+
+impl Protocol {
+    pub fn default_port(&self) -> u16 {
+        match self {
+            Protocol::Http => 80,
+            Protocol::Https => 443,
+            _ => panic!("Partial function"),
+        }
+    }
 }
